af33ed6026894516486518c8be280e422f876e6f
KristinaHejlesen <kkhejlesen@gmail.com>
2025-09-13 23:00:22 +0200

    test af nyt post type

--- alimento-post-types.php
+++ alimento-post-types.php
@@ -19,6 +19,23 @@
         ),
         'menu_icon' => 'dashicons-admin-customizer'
     ));
+
+    register_post_type('book', array(
+        'supports' => array('title', 'editor', 'excerpt', 'thumbnail'),
+        'rewrite' => array('slug' => 'book'),
+        'has_archive' => true,
+        'public' => true,
+        'show_in_rest' => true,
+        'labels' => array(
+            'name' => 'Bookd',
+            'add_new_item' => 'Add New Book',
+            'edit_item' => 'Edit Book',
+            'all_items' => "All Bookd",
+            'singular_name' => 'Book'
+
+        ),
+        'menu_icon' => 'dashicons-admin-book'
+    ));
 }
 //vi kalder funktionen blog_post_types
 add_action('init', 'blog_post_types');
