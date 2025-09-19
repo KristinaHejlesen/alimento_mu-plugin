14e74008a2ce7aaf7dbaeafcfe72b126ac117298
KristinaHejlesen <kkhejlesen@gmail.com>
2025-09-19 12:30:43 +0200

    tilføjet tilladelse til members i mu plugins

--- alimento-post-types.php
+++ alimento-post-types.php
@@ -4,7 +4,11 @@
     // Blog post types _ Vi øver os i at lave post-types, man kunne sagtens bare anvend post i wordpress!
     //husk register_post_type('blog', = navnet på single-blog.php skal være det samme
     register_post_type('blog', array(
-        'supports' => array('title', 'editor', 'excerpt', 'thumbnail'),
+        //opfører sig ikke som posttype når det gælder vores plugin members, laver sin egen unikke tilladelser/egenskaber. per deault er denne sat til posts.
+        'capability_type' => 'blog',
+        // denne laves fordi, den giver wp besked om blog skal have tilladse til at give capability-type = blog
+        'map_meta_cap' => true,
+        'supports' => array('title', 'excerpt', 'thumbnail'),
         'rewrite' => array('slug' => 'blog'),
         'has_archive' => true,
         'public' => true,
@@ -21,7 +25,9 @@
     ));
 
     register_post_type('recipe', array(
-        'supports' => array('title', 'excerpt', 'thumbnail'),
+        'capability_type' => 'recipe',
+        'map_meta_cap' => true,
+        'supports' => array('title', 'excerpt', 'thumbnail',),
         'rewrite' => array('slug' => 'recipe'),
         'has_archive' => true,
         'public' => true,
